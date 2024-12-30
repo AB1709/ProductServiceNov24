5cb07da5d439cf73fbbc0cbaf73e9e7848ae095a
aNzi <46045938+AB1709@users.noreply.github.com>
2024-12-30 06:09:22 +0530

    Adding GetAllProducts and ReplaceProduct call using FakeStore API

--- src/main/java/com/example/productservicenov24/controllers/ProductController.java
+++ src/main/java/com/example/productservicenov24/controllers/ProductController.java
@@ -2,10 +2,9 @@ package com.example.productservicenov24.controllers;
 
 import com.example.productservicenov24.models.Product;
 import com.example.productservicenov24.services.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -15,8 +14,17 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long id){
+    public Product getProductById(@PathVariable("id") Long id) {
         return productService.getProductById(id);
+    }
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        return productService.replaceProduct(id, product);
     }
 }
--- src/main/java/com/example/productservicenov24/services/FakeStoreProductService.java
+++ src/main/java/com/example/productservicenov24/services/FakeStoreProductService.java
@@ -4,8 +4,16 @@ package com.example.productservicenov24.services;
 import com.example.productservicenov24.dtos.FakeStoreProductsDto;
 import com.example.productservicenov24.models.Category;
 import com.example.productservicenov24.models.Product;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class FakeStoreProductService implements ProductService {
@@ -14,11 +22,37 @@ public class FakeStoreProductService implements ProductService {
     public FakeStoreProductService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
+
     @Override
-    public Product getProductById(Long id){
-        FakeStoreProductsDto fakeStoreProductsDto =  restTemplate.getForObject("https://fakestoreapi.com/products/"+id, FakeStoreProductsDto.class);
+    public Product getProductById(Long id) {
+        FakeStoreProductsDto fakeStoreProductsDto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductsDto.class);
         return convertFakeStoreProductsDtoToProduct(fakeStoreProductsDto);
     }
+
+    public List<Product> getAllProducts() {
+        FakeStoreProductsDto[] fakeStoreProductsDtoList = restTemplate.getForObject("https://fakestoreapi.com/products", FakeStoreProductsDto[].class);
+        List<Product> products = new ArrayList<>();
+        assert fakeStoreProductsDtoList != null;
+        for (FakeStoreProductsDto fakeStoreProductsDto : fakeStoreProductsDtoList) {
+            products.add(convertFakeStoreProductsDtoToProduct(fakeStoreProductsDto));
+        }
+        return products;
+    }
+
+    @Override
+    public Product replaceProduct(Long id, Product product) {
+        FakeStoreProductsDto fakeStoreProductsDto = new FakeStoreProductsDto();
+        fakeStoreProductsDto.setId(product.getId());
+        fakeStoreProductsDto.setTitle(product.getTitle());
+        fakeStoreProductsDto.setDescription(product.getDescription());
+        fakeStoreProductsDto.setPrice(product.getPrice());
+        RequestCallback requestCallback = restTemplate.httpEntityCallback(fakeStoreProductsDto);
+        ResponseExtractor<ResponseEntity<FakeStoreProductsDto>> responseExtractor = restTemplate.responseEntityExtractor(FakeStoreProductsDto.class);
+        FakeStoreProductsDto fakeStoreProductsDto1 = Objects.requireNonNull(restTemplate.execute("https://fakestoreapi.com/products/" + id, HttpMethod.PUT, requestCallback, responseExtractor)).getBody();
+        return convertFakeStoreProductsDtoToProduct(fakeStoreProductsDto1);
+    }
+
+
     private Product convertFakeStoreProductsDtoToProduct(FakeStoreProductsDto fakeStoreProductsDto) {
         if (fakeStoreProductsDto == null) {
             return null;
